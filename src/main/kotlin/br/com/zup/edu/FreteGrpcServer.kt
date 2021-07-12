5dfe38449855185a5f374087dbfcfdd5ea39000f
William Bohn <williamxavero@gmail.com>
2021-07-12 11:59:22 -0300

    Retornando códigos de erros

--- src/main/kotlin/br/com/zup/edu/FreteGrpcServer.kt
+++ src/main/kotlin/br/com/zup/edu/FreteGrpcServer.kt
@@ -1,5 +1,9 @@
 package br.com.zup.edu
 
+import com.google.protobuf.Any
+import com.google.rpc.Code
+import io.grpc.Status
+import io.grpc.protobuf.StatusProto
 import io.grpc.stub.StreamObserver
 import org.slf4j.LoggerFactory
 import javax.inject.Singleton
@@ -12,9 +16,58 @@ class FreteGrpcServer : FreteZupServiceGrpc.FreteZupServiceImplBase() {
     override fun calculaFrete(request: FreteRequest?, responseObserver: StreamObserver<FreteResponse>?) {
         logger.info("Calculando o frete para o cep ${request!!.cep}")
 
+        val cep = request?.cep
+        if (cep == null || cep.isBlank()) {
+            val e = Status.INVALID_ARGUMENT
+                .withDescription("É preciso informar o cep")
+                .asRuntimeException()
+            responseObserver?.onError(e)
+        }
+
+        if (!cep!!.matches("[0-9]{5}-[0-9]{3}".toRegex())) {
+            val e = Status.INVALID_ARGUMENT
+                .withDescription("Cep inválido")
+                .augmentDescription("Formato de cep inválido! Formato esperado 00000-000")
+                .asRuntimeException()
+            responseObserver?.onError(e)
+        }
+
+        var valor = 0.0
+        try {
+            valor = Random.nextDouble(0.0, 140.0)
+            if (valor > 100.0) {
+                throw IllegalStateException("Erro inesperado")
+            }
+        } catch (e: Exception) {
+            responseObserver?.onError(
+                Status.INTERNAL
+                    .withDescription(e.message)
+                    .withCause(e) // anexado ao Status, mas não enviado ao Client
+                    .asRuntimeException()
+            )
+        }
+
+        //SIMULAR uma verificação de segurança
+        if (cep.endsWith("333")) {
+            val statusProto = com.google.rpc.Status.newBuilder()
+                .setCode(Code.PERMISSION_DENIED.number)
+                .setMessage("Usuário não pode acessar esse recurso")
+                .addDetails(
+                    Any.pack(
+                        ErroDetails.newBuilder()
+                            .setCode(401)
+                            .setMessage("Token expirado")
+                            .build()
+                    )
+                )
+                .build()
+            val e = StatusProto.toStatusRuntimeException(statusProto)
+            responseObserver?.onError(e)
+        }
+
         val response = FreteResponse.newBuilder()
             .setCep(request!!.cep)
-            .setValor(Random.nextDouble(0.0, 140.0))
+            .setValor(valor)
             .build()
 
         logger.info("Frete calculado: valor $response")
